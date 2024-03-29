88bcbc814d0853fddf5ca4dca5c8a1c8549fea06
Cody Keener <codymkeener@gmail.com>
2024-03-29 12:25:05 -0500

    Add localStorage feature to retain values after refresh and add tab button to save current tab

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el", "inputEl");
 const inputBtn = document.getElementById("input-btn", "inputBtn");
 const ulEl = document.getElementById("ul-el");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    renderLeads();
-    inputEl.value = "";
-    console.log(myLeads);
-})
+/* In the inputBtn even listener below, the myLeads array gets added to localStorage. This line checks if there is anything
+in localStorage and if so, adds that value to the listItems */
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
-function renderLeads() {
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
+}
+
+function render(leads) {
     // ulEl.innerHTML = "";
 
     // This was my first idea for this
@@ -27,7 +31,7 @@ function renderLeads() {
 
     // And this is the way Scrimba said to do it
     let listItems = "";
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
             listItems += `
                 <li>
                     <a target='_blank' rel='noreferrer noopener' href='https://${myLeads[i]}'>
@@ -39,3 +43,29 @@ function renderLeads() {
 
     ulEl.innerHTML = listItems;
 }
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+    console.log(localStorage.getItem("myLeads"));
+})
+
+deleteBtn.addEventListener("dblclick", function() {
+    myLeads = [];
+    localStorage.clear();
+    render(myLeads);
+})
+
+tabBtn.addEventListener("click", function() {
+    //I found this method when I Googled. This will work in a browser window but not in a Chrome Extension
+    //myLeads.push(window.location.href);
+
+    //but Scrimba saidto do it this way because of the way Chrome works
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    })
+})
